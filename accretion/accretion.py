9d85eea3441a464c2f335547ea7857d730866a6e
chamberse <chambers.e2002@gmail.com>
2026-06-24 13:09:54 -0700

    make variables from file integers

--- accretion/accretion.py
+++ accretion/accretion.py
@@ -1,13 +1,17 @@
 """this code uses user input and Eq 1 from Kummer (2025) to determine what
  kind of BBH accretion will occur in a given system."""
 
-import numpy as np
-
-
 with open('initial_conditions.txt', 'r') as file:
     mstar, mbh1, mbh2, aout, eout, ain, ein = file.read().splitlines()
+    print(mstar, mbh1, mbh2, aout, eout, ain, ein)
 
-qout = mstar / (mbh1 + mbh2)
+mstar = int(mstar)
+mbh1 = int(mbh1)
+mbh2 = int(mbh2)
+aout = int(aout)
+eout = int(eout)
+ain = int(ain)
+ein = int(ein)
 
 #unit conversions
 #mstar = mstari * 1.989*10**30
@@ -16,20 +20,24 @@ qout = mstar / (mbh1 + mbh2)
 #aout = aouti * 6.957*10**8
 #ain = aini * 6.957*10**8
 
+qout = mstar / (mbh1 + mbh2)
+
 rmin = 0.425 * aout * (1 - eout)*((1/qout)*(1+1/qout))
 
 aapo = ain * (1 + ein)
 
 if aapo > rmin:
-    print(f"{aapo:e} is greater than {rmin:e}")
+    print(r"{aapo:e}$R_{\odot}$ is greater than {rmin:e}$R_{\odot}$")
     print("Ballistic accretion will occur in this system.")
 
 if rmin > aapo:
-    print(f"{aapo:e} is less than {rmin:e}")
+    print(r"{aapo:e}$R_{\odot}$ is less than {rmin:e}$R_{\odot}$")
     print("A circumbinary disk will form in this system.")
 else:
     print("Error")
 
 # eventually add plot with projected accretion path
 # use more robust calculations with modules and functions
+# calculate the critical value for BA accretion
+
 
