3fd7821698a0767e8064601cbb0385eec4eb50a7
chamberse <chambers.e2002@gmail.com>
2026-06-24 10:33:40 -0700

    added init and pyproject files

--- cbd_or_ba.py
+++ cbd_or_ba.py
@@ -2,21 +2,22 @@
  kind of BBH accretion will occur in a given system."""
 
 
-mstari = float(input("Enter the mass of the star (in Msun): ")) #convert to kg later
-mbh1i = float(input("Enter the mass of BH 1 (in Msun): ")) #convert to kg later
-mbh2i = float(input("Enter the mass of BH 2 (in Msun): ")) #convert to kg later
-aouti = float(input("Enter distance between star and COM of BBH (in Rsun): ")) #convert to meters later
+mstar = float(input("Enter the mass of the star (in Msun): ")) #convert to kg later
+mbh1 = float(input("Enter the mass of BH 1 (in Msun): ")) #convert to kg later
+mbh2 = float(input("Enter the mass of BH 2 (in Msun): ")) #convert to kg later
+aout = float(input("Enter distance between star and COM of BBH (in Rsun): ")) #convert to meters later
 eout = float(input("Enter the eccentricity of the BBH and star orbit: "))
-aini = float(input("Enter the distance between the BHs (in Rsun):" )) #convert to meters later
+ain = float(input("Enter the distance between the BHs (in Rsun):" )) #convert to meters later
 ein = float(input("Enter the eccentricity of the BBH orbit: "))
 
-qout = mstari / (mbh1i + mbh2i)
+qout = mstar / (mbh1 + mbh2)
 
-mstar = mstari * 1.989*10**30
-mbh1 = mbh1i * 1.989*10**30
-mbh2 = mbh2i * 1.989*10**30
-aout = aouti * 6.957*10**8
-ain = aini * 6.957*10**8
+#unit conversions
+#mstar = mstari * 1.989*10**30
+#mbh1 = mbh1i * 1.989*10**30
+#mbh2 = mbh2i * 1.989*10**30
+#aout = aouti * 6.957*10**8
+#ain = aini * 6.957*10**8
 
 rmin = 0.425 * aout * (1 - eout)*((1/qout)*(1+1/qout))
 
@@ -31,4 +32,7 @@ if rmin > aapo:
     print("A circumbinary disk will form in this system.")
 else:
     print("Error")
+
+# eventually add plot with projected accretion path
+# use more robust calculations with modules
 
