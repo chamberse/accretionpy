1167a67c1dfaa01069a0df19ac38bfc8b6a53561
chamberse <chambers.e2002@gmail.com>
2026-06-24 13:58:44 -0700

    fixes

--- accretion/accretion.py
+++ accretion/accretion.py
@@ -1,9 +1,11 @@
 """this code uses user input and Eq 1 from Kummer (2025) to determine what
  kind of BBH accretion will occur in a given system."""
 
+import numpy as np
+
 with open('initial_conditions.txt', 'r') as file:
     mstar, mbh1, mbh2, aout, eout, ain, ein = file.read().splitlines()
-    print(mstar, mbh1, mbh2, aout, eout, ain, ein)
+    #print(mstar, mbh1, mbh2, aout, eout, ain, ein)
 
 mstar = int(mstar)
 mbh1 = int(mbh1)
@@ -26,15 +28,32 @@ rmin = 0.425 * aout * (1 - eout)*((1/qout)*(1+1/qout))
 
 aapo = ain * (1 + ein)
 
+print(aapo)
+
+acirc = (0.5 - 0.0986*np.log(qout))**4*(1+qout)
+print(acirc)
+acav = 1.6*acirc
+print(acav)
+#since acav / abin = 1.68, solve for abin
+
+abin = acav / 1.68
+print(abin)
+
 if aapo > rmin:
-    print(r"{aapo:e}$R_{\odot}$ is greater than {rmin:e}$R_{\odot}$")
+    print(f"{aapo:e} is greater than {rmin:e}")
     print("Ballistic accretion will occur in this system.")
 
 if rmin > aapo:
-    print(r"{aapo:e}$R_{\odot}$ is less than {rmin:e}$R_{\odot}$")
+    print(f"{aapo:e} is less than {rmin:e}")
     print("A circumbinary disk will form in this system.")
 else:
     print("Error")
+
+if abin != aapo:
+    print(f"binary sparations not equal: {abin, aapo}")
+
+
+
 
 # eventually add plot with projected accretion path
 # use more robust calculations with modules and functions
