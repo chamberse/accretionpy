807714b45694636d99710d9f1be7c62ebfe90cbb
Emma <chambers.e2002@gmail.com>
2026-06-29 11:32:15 -0700

    accretion.py works

--- accretionpy/accretion.py
+++ accretionpy/accretion.py
@@ -14,7 +14,6 @@ the mass stream can approach the accreting binary.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from ipywidgets import interact
 
 def read_file():    
     """
@@ -27,14 +26,27 @@ def read_file():
             #line = [int(line.strip()) for line in file if line.strip()]
             if not line.strip() or line.startswith("#"):
                 continue
-            
-            # Split at the first '=' sign
-            key, value = line.split(" = ", 1)
+            if '=' in line:
+                # Split at the first '=' sign
+                key, value = line.split("=", 1)
         
             # Strip whitespace and save to dictionary
-            initial_conditions[key.strip()] = int(value.strip())
+            initial_conditions[key.strip()] = float(value.strip())
 
     return initial_conditions
+
+def read_variables(initial_conditions):
+    mstar = initial_conditions['mstar']
+    mbh1 = initial_conditions['mbh1']
+    mbh2 = initial_conditions['mbh2']
+    aout = initial_conditions['aout']
+    eout = initial_conditions['eout']
+    ein = initial_conditions['ein']
+
+    #allows in to be 0 or None in text file
+    ain = initial_conditions.get('ain', None) 
+
+    return mstar, mbh1, mbh2, aout, eout, ain, ein
 
 def calculate_rmin(mstar, mbh1, mbh2, aout, eout):
     """
@@ -48,36 +60,54 @@ def calculate_rmin(mstar, mbh1, mbh2, aout, eout):
     else:
         rmin = 0.425 * aout * (1 - eout)*((1/qout)*(1+1/qout))
 
-    return rmin
+    return qout, rmin
 
-def calculate_aapo(ain, ein, ain_crit, rmin):
+def calculate_aapo(ain, ein, rmin):
     """
     Docstring here
     """
-    if not ain:
-        print("Calculating critical separation required for ballistic accretion...")
+    if ain is None or ain == 0:
+        aapo = None
         ain_crit = rmin + 1
     else:
         aapo = ain * (1 + ein)
+        ain_crit = rmin + 1
+
     return aapo, ain_crit
 
-def determine_accretion_type(aapo, rmin):
+def determine_accretion_type(aapo, rmin, ain_crit):
     """
     
     """
+    if aapo is None:
+        print(f"The minimum inner binary separation (ain) needed to achieve ballistic accretion is {ain_crit:.2f} Rsun.")
+        return
 
     if aapo > rmin:
-        print(f"{aapo:.2f} Rsun is greater than {rmin:.2f} Rsun")
         print("Ballistic accretion will occur in this system.")
-
-    if rmin > aapo:
-        print(f"{aapo:.2f} Rsun is less than {rmin:.2f} Rsun")
+        print(f"The minimum separation to acheive ballistic accretion is {ain_crit:.2f} Rsun.")
+    else:
         print("A circumbinary disk will form in this system.")
+        print(f"The minimum separation to acheive ballistic accretion is {ain_crit:.2f} Rsun.")
 
 def interactive_plot():
     """
     
     """
+
+def main():
+    initial_conditions = read_file()
+    mstar, mbh1, mbh2, aout, eout, ain, ein = read_variables(initial_conditions)
+    qout, rmin = calculate_rmin(mstar, mbh1, mbh2, aout, eout)
+    aapo, ain_crit = calculate_aapo(ain, ein, rmin)
+    output = determine_accretion_type(aapo, rmin, ain_crit)
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
+
 
 #acirc = (aout*(0.5 - 0.0986*np.log(qout))**4)*(1+qout)
 #print(acirc)
